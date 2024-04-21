e5eae6b67fecbe328d6752cf564b9ff4defb25a5
Tyrone Francis <tyrone.h.francis@gmail.com>
2024-04-21 11:16:00 +0000

    added add_task.html and functionality

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,9 +1,21 @@
 document.addEventListener('DOMContentLoaded', function() {
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
-  });
 
-  document.addEventListener('DOMContentLoaded', function() {
+    // modal
     var elems = document.querySelectorAll('.modal');
     var instances = M.Modal.init(elems);
- });
+
+    // datepicker initialization
+    let datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+      format: "dd mmmm, yyyy",
+      i18n: {done:"Select"}
+    });
+
+    // select initialization
+    let selects = document.querySelectorAll('select');
+    M.FormSelect.init(selects);
+  });
+
+ 
